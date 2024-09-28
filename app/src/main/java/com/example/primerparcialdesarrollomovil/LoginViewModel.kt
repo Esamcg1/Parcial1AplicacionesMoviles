1e0ecd5c3806c19ea881bb3b6d416aa577f393a0
sam <chugasamuel@gmail.com>
2024-09-27 21:49:14 -0600

    segunda subida: logica para login

---
+++ app/src/main/java/com/example/primerparcialdesarrollomovil/LoginViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.primerparcialdesarrollomovil
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel {
+    var username: String = ""
+    var password: String = ""
+
+    fun login(): Boolean{
+        return username == "rgonzaleza" && password == "rgonzaleza"
+    }
+}
